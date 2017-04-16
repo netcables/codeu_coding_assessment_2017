449c438fcf4427ce9ece6f1e22ed7f2b1b07a2a7
Antonio Carmona <adcarmona98@gmail.com>
2017-04-16 11:10:45 -0700

    Switched to using stacks.
    Going to try using what I actually know about rather than trying to
    overcomplicate things.

--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -15,40 +15,107 @@
 package com.google.codeu.codingchallenge;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.Stack;
 
 final class MyJSONParser implements JSONParser {
-
-    
+	
+	// Input string "in" converted to stack
+	private Stack<Object> inputStringStack = new Stack<Object>();
+	
   @Override
   public JSON parse(String in) throws IOException {
-    // TODO: implement this
-	  System.out.println(in);
+	
+	// JSON object to be returned
+	MyJSON returnJSON = new MyJSON();
+	  
+	// boolean used to check if within quotes
+	boolean withinQuotes = false;
+	
+	// temporary string used to hold string values
+	String tempString = new String();
+	
+	// iterate through string
+	for (int i = 0; i < in.length(); i++) {
+		// if the character at index i is...
+		switch(in.charAt(i)) {
+			// if quotes, mark that scanning through quote
+			case '"':
+				if (withinQuotes == false) {
+					withinQuotes = true;
+					break;
+				}
+				// if already in quotes, push string into stack
+				else {
+					withinQuotes = false;
+				    inputStringStack.push(tempString);
+					tempString = new String();
+					break;
+				}
+			// if opening bracket, push into stack
+			case '{':
+				//inputStringStack.push('{');
+				break;
+			// if closing bracket, evaluate stack until '{'
+			case '}':
+				returnJSON = (MyJSON)evaluate();
+				break;
+			// if colon, push into stack
+			case ':':
+				inputStringStack.push(':');
+				break;
+			// if comma, push into stack
+			case ',':
+				inputStringStack.push(',');
+				break;
+			// if in quotes, add char to tempstring
+			default:
+				if (withinQuotes == true) {
+					tempString += in.charAt(i);
+					break;
+				}
+		}
+	}
+	return returnJSON;
+  }
+  
+  private JSON evaluate() {
+	  
+	  // new myJSON object
 	  MyJSON newJSON = new MyJSON();
+	  // temporary JSON object
+	  MyJSON tempJSON = new MyJSON();
+	  // temporary value string
+	  String newValue = new String();
+	  //temporary key string
+	  String newKey = new String();
+	  //check if value is object
+	  boolean isObject = false;
 	  
-	  Pattern quotePattern = Pattern.compile("\"([^\"]*)\"");
-	  Matcher quoteMatcher = quotePattern.matcher(in);
-	  
-	  String newKey = new String("");
-	  String newValue = new String("");
-	  
-	  while(quoteMatcher.find()) {
-		  if(newKey.equals("")) {
-			  newKey = quoteMatcher.group(1);
-		  }
-		  else if(newValue.equals("")) {
-			  newValue = quoteMatcher.group(1);
-		  }
-		  else {
-			  newJSON.setString(newKey, newValue);
-			  newKey = "";
-			  newValue = "";
-		  }
+	  if(inputStringStack.isEmpty()) {
+		  return newJSON;
 	  }
-	newJSON.setString(newKey, newValue);
-	newKey = "";
-	newValue = "";
-    return newJSON;
+	  // the top of the stack should be a value
+	  if(inputStringStack.peek() instanceof String) {
+		System.out.println(inputStringStack.peek());
+		  newValue = (String)inputStringStack.pop();
+	  }
+	  else {
+		  isObject = true;
+		  tempJSON = (MyJSON)inputStringStack.pop();
+	  }
+	  // the next item should be a colon
+	  if((char)inputStringStack.pop() == ':') {
+		System.out.println(inputStringStack.peek());
+		  newKey = (String)inputStringStack.pop();
+	  }
+	  // if the value is a JSON object, setObject
+	  if(isObject == true) {
+		  newJSON.setObject(newKey, tempJSON);
+	  }
+	  // otherwise, setString
+	  else {
+		  newJSON.setString(newKey, newValue);
+	  }
+	return newJSON;
   }
 }
